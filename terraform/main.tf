d05794d33481204363bf73a0a750b8b1b32ed10b
RaytraceSG <azmi.maniku@gmail.com>
2024-10-30 22:36:23 +0900

    fix checkov

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,9 +51,16 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3_lifecycle_config" {
 resource "aws_instance" "ec2_example" {
   ami           = data.aws_ami.aws_ami_data.id
   instance_type = "t3.micro"
+  ebs_optimized = true
+  monitoring    = true
+  metadata_options {
+    http_endpoint = "disabled"
+  }
   tags = {
     Name = "azmi1-terraform-ec2-cicd"
   }
+  #checkov:skip=CKV2_AWS_41:Skip IAM role
+  #checkov:skip=CKV_AWS_8:No launch configuration
 }
 
 data "aws_ami" "aws_ami_data" {
