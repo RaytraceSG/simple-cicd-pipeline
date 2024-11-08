514d87dbdb785c511d43828b26a5e1ea4b513b73
RaytraceSG <azmi.maniku@gmail.com>
2024-11-08 09:41:01 +0900

    update compute.tf

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "ec2_example" {
   ami                    = data.aws_ami.aws_ami_data.id
   key_name               = "azmi1-keypair-useast1"
   monitoring             = true
-  vpc_security_group_ids = [aws_security_group.azmi1-tf-sg-allow-ssh-http-https]
-  subnet_id              = module.vpc.public_subnets[0].id
+  vpc_security_group_ids = [aws_security_group.azmi1-tf-sg-allow-ssh-http-https.id]
+  subnet_id              = module.vpc.public_subnets[0]
   instance_type          = "t3.micro"
   ebs_optimized          = true
   metadata_options {
