a6ad3a4acc8cb1db28606a0c731c6d122cb99cdd
RaytraceSG <azmi.maniku@gmail.com>
2024-11-08 09:55:52 +0900

    change ec2 resource to module

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,16 +1,19 @@
-resource "aws_instance" "ec2_example" {
-  ami                    = data.aws_ami.aws_ami_data.id
-  key_name               = "azmi1-keypair-useast1"
-  monitoring             = true
-  vpc_security_group_ids = [aws_security_group.azmi1-tf-sg-allow-ssh-http-https.id]
-  subnet_id              = module.vpc.public_subnets[0]
-  instance_type          = "t3.micro"
-  ebs_optimized          = true
-  metadata_options {
-    http_endpoint = "disabled"
-  }
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.7.0"
+
+  name                        = var.ec2_name
+  ami                         = data.aws_ami.aws_ami_data.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  monitoring                  = true
+  vpc_security_group_ids      = [aws_security_group.azmi1-tf-sg-allow-ssh-http-https.id]
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  ebs_optimized               = true
+
   tags = {
-    Name = "azmi1-terraform-ec2-cicd"
+    Name = var.ec2_name
   }
   #checkov:skip=CKV2_AWS_41:Skip IAM role
   #checkov:skip=CKV_AWS_8:No launch configuration
