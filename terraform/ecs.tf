10d194d96d18d96334b53600d08e60378ded5189
RaytraceSG <azmi.maniku@gmail.com>
2024-11-08 10:49:55 +0900

    update ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -12,29 +12,34 @@ resource "aws_ecs_task_definition" "nginx_task" {
   family                   = var.ecs_task_def_family
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
+  cpu                      = "1024"
+  memory                   = "3072"
 
   container_definitions = jsonencode([
     {
-      name  = "azmi1-nginx-container-1"
+      name  = "azmi1-nginx-container"
       image = "nginx:latest"
       portMappings = [
         {
+          name = "nginx80-tcp"
           containerPort = 80
           hostPort      = 80
+          protocol      = "tcp"
+          appProtocol   = "http"
         }
       ]
-    },
-    {
-      name  = "azmi1-nginx-container-2"
-      image = "nginx:latest"
-      portMappings = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
-        }
-      ]
+      essential = true
+      logConfiguration: {
+          logDriver: "awslogs"
+          options: {
+            awslogs-group: "/ecs/azmi1-taskdef-test"
+            mode: "non-blocking"
+            awslogs-create-group: "true"
+            max-buffer-size: "25m"
+            awslogs-region: "us-east-1"
+            awslogs-stream-prefix: "ecs"
+          }
+      }
     }
   ])
   #checkov:skip=CKV_AWS_336:Ensure ECS containers are limited to read-only access to root filesystems
@@ -51,18 +56,13 @@ resource "aws_ecs_service" "nginx_service" {
   network_configuration {
     subnets          = module.vpc.public_subnets # Replace with your subnet IDs
     assign_public_ip = true
+    security_groups = [aws_security_group.azmi1-tf-sg-allow-ssh-http-https.id]
   }
 
   load_balancer {
     target_group_arn = aws_lb_target_group.nginx_tg.arn
-    container_name   = "azmi1-nginx-container-1"
+    container_name   = "azmi1-nginx-container"
     container_port   = 80
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.nginx_tg_2.arn
-    container_name   = "azmi1-nginx-container-2"
-    container_port   = 8080
   }
   #checkov:skip=CKV_AWS_333:Ensure ECS services do not have public IP addresses assigned to them automatically
 }
@@ -95,22 +95,6 @@ resource "aws_lb_listener" "front_end" {
   #checkov:skip=CKV_AWS_103:Ensure that load balancer is using at least TLS 1.2
 }
 
-resource "aws_lb_listener_rule" "nginx_rule_2" {
-  listener_arn = aws_lb_listener.front_end.arn
-  priority     = 200
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.nginx_tg_2.arn
-  }
-
-  condition {
-    path_pattern {
-      values = ["/api/*"] # Adjust this path as needed
-    }
-  }
-}
-
 # Target Group
 resource "aws_lb_target_group" "nginx_tg" {
   name        = "azmi1-nginx-tg"
@@ -121,26 +105,7 @@ resource "aws_lb_target_group" "nginx_tg" {
 
   health_check {
     healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "200"
-    timeout             = "3"
-    path                = "/"
-    unhealthy_threshold = "2"
-  }
-  #checkov:skip=CKV_AWS_378:Ensure AWS Load Balancer doesn't use HTTP protocol
-}
-
-resource "aws_lb_target_group" "nginx_tg_2" {
-  name        = "azmi1-nginx-tg-2"
-  port        = 8080
-  protocol    = "HTTP"
-  vpc_id      = module.vpc.vpc_id
-  target_type = "ip"
-
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
+    interval            = "240"
     protocol            = "HTTP"
     matcher             = "200"
     timeout             = "3"
