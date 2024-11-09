4a862ed8593d58ac36a9f22ca09cdc5367add313
RaytraceSG <azmi.maniku@gmail.com>
2024-11-09 11:20:52 +0900

    edit variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -16,20 +16,20 @@ variable "sg_name" {
   default     = "azmi1-tf-sg-allow-ssh-http-https"
 }
 
-variable "key_name" {
-  description = "Name of EC2 Key Pair"
-  type        = string
-  default     = "azmi1-keypair-useast1-1" # Replace with your own key pair name (without .pem extension) that you have downloaded from AWS console previously
-}
+# variable "key_name" {
+#   description = "Name of EC2 Key Pair"
+#   type        = string
+#   default     = "azmi1-keypair-useast1-1" # Replace with your own key pair name (without .pem extension) that you have downloaded from AWS console previously
+# }
 
-variable "ec2_name" {
-  description = "Name of EC2"
-  type        = string
-  default     = "azmi1-ec2-tf-cicd" # Replace with your preferred EC2 Instance Name 
-}
+# variable "ec2_name" {
+#   description = "Name of EC2"
+#   type        = string
+#   default     = "azmi1-ec2-tf-cicd" # Replace with your preferred EC2 Instance Name 
+# }
 
-variable "instance_type" {
-  description = "EC2 Instance type"
-  type        = string
-  default     = "t2.micro"
-}
+# variable "instance_type" {
+#   description = "EC2 Instance type"
+#   type        = string
+#   default     = "t2.micro"
+# }
