b41109c898ca520628dbfc26b92195149cea6934
RaytraceSG <azmi.maniku@gmail.com>
2024-11-09 11:58:36 +0900

    edit data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,25 +1,25 @@
-data "aws_ami" "aws_ami_data" {
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "aws_ami_data" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["al2023-ami-2023*"]
+#   }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
+#   filter {
+#     name   = "root-device-type"
+#     values = ["ebs"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
+#   filter {
+#     name   = "architecture"
+#     values = ["x86_64"]
+#   }
 
-}
+# }
