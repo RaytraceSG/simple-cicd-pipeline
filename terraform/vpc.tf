38f244091662fef00704883266e872be9b769a11
RaytraceSG <azmi.maniku@gmail.com>
2024-11-11 18:32:40 +0800

    update destroy workflow and vpc name variable

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,21 +2,15 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.15.0"
 
-  name = "azmi1-vpc-tf-module"
+  name = "${var.name_prefix}-vpc-${terraform.workspace}"
   cidr = "10.0.0.0/16"
 
   azs                  = ["us-east-1a", "us-east-1b"]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_names = ["azmi1-tf-private-subnet-az1", "azmi1-tf-private-subnet-az2"]
+  private_subnet_names = ["${var.name_prefix}-tf-private-subnet-az1-${terraform.workspace}", "${var.name_prefix}-tf-private-subnet-az2-${terraform.workspace}"]
   public_subnets       = ["10.0.101.0/24", "10.0.102.0/24"]
-  public_subnet_names  = ["azmi1-tf-public-subnet-az1", "azmi1-tf-public-subnet-az2"]
+  public_subnet_names  = ["${var.name_prefix}-tf-public-subnet-az1-${terraform.workspace}", "${var.name_prefix}-tf-public-subnet-az2-${terraform.workspace}"]
 
-
-  tags = {
-    Terraform  = "true"
-    Created_by = "Azmi"
-    Cohort     = "CE7"
-  }
   #checkov:skip=CKV_TF_1:Ensure Terraform module sources use a commit hash
 }
 
